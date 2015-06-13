901c0296387c863f90b1771cd1cc5fb447c7b0da
Anne Noble <devney@stanford.edu>
2015-06-13 16:40:41 -0700

    basic functionality - picks a random student.

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,44 @@
-console.log("Hi")
-alert("hi!")
+var myStudents = [
+"Yara"
+, "Christy"
+, "Anusha"
+, "Sandra"
+, "Serene"
+, "Jozlin"
+, "Maya G"
+, "Neha"
+, "Hayley"
+, "Anya"
+, "Erika"
+, "Michelle"
+, "Serena"
+, "Maya M"
+, "Kelela"
+, "Adhayana"
+, "Alexis"
+, "Madison"
+, "Emily"
+, "Jessica"
+];
+
+var pickStudent = function(students){
+    //var randomIndex = Math.floor(Math.random() * students.length)
+    //var theChosenOne = students[randomIndex]
+    var theChosenOne = _.sample(students);
+    var htmlString = theChosenOne + "!"
+    $("#chosen-student-container").html(htmlString)
+}
+
+var pickFromMyStudents = function(){
+    pickStudent(myStudents);
+};
+
+var reset = function(){
+	myStudents = _.shuffle(myStudents);
+     $("#chosen-student-container").html("?");
+};
+
+
+                                         
+$("#pick-student-button").click(pickFromMyStudents)
+$("#reset-button").click(reset)
